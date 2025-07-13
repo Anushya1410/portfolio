b5c4ac2c118ff38ab8226404c9f446c577e3073c
Anushya1410 <your.email@example.com>
2025-07-13 18:27:38 +0530

    fix: update vite base, package.jsone

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Anushya1410/portfolio.git', // Replace with your actual repo name
+  base: '/portfolio/', // 👈 Your GitHub repo name
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
